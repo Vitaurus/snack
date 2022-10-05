16a0903fff434e0498a76ec316788e4e84506782
Віталій Талько <cotleopol1d@gmail.com>
2022-10-05 21:49:54 +0300

    game make

---
+++ js/modules/variables.js
@@ -0,0 +1,11 @@
+export const canvas = document.querySelector("#canvas");
+export const canvasWidth = canvas.width =  window.innerWidth-4;
+const x = canvasWidth*0.0091;
+export const radius = canvasWidth/(Math.round(canvasWidth/x));
+let y = Math.floor((window.innerHeight-4)/(radius*2));
+if (y%2===0){
+    y=y-1;
+}
+export const canvasHeight = canvas.height = y*2*(radius);
+export const ctx = canvas.getContext("2d");
+export let acceleration = 0.1;
