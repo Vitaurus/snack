fb2878ff38991abff36e3473f2b0d407c00a3699
Віталій Талько <cotleopol1d@gmail.com>
2022-10-11 15:57:35 +0300

    touch move fix3

--- js/main.js
+++ js/main.js
@@ -32,15 +32,17 @@ class Game {
         this.touchHelperDraw();
         this.arraySnackManage(this.x, this.y, this.x, this.y);
     }
-    calcArrow(){
-    const x = Math.round(this.canvasWidth/(radius*2));
-    const y = Math.round(this.canvasHeight/(radius*2));
-    const arrow = x*y;
-        this.coeff = (Math.abs(0.1-this.acceleration))/arrow;
+
+    calcArrow() {
+        const x = Math.round(this.canvasWidth / (radius * 2));
+        const y = Math.round(this.canvasHeight / (radius * 2));
+        const arrow = x * y;
+        this.coeff = (Math.abs(0.1 - this.acceleration)) / arrow;
     }
-    arraySnackManage(x, y, newX, newY, push = false, check = false, index = [1,1,1,1], meal = this.meal) {
+
+    arraySnackManage(x, y, newX, newY, push = false, check = false, index = [1, 1, 1, 1], meal = this.meal) {
         if (check) {
-            return this.arrayOfPixels.find(({x, y}, key) => Math.floor(meal.x) + Math.floor(this.radius) > Math.ceil(x) - Math.floor(this.radius) && Math.ceil(meal.x) - Math.floor(this.radius) < Math.floor(x) + Math.floor(this.radius) && Math.floor(meal.y) + Math.floor(this.radius) > Math.ceil(y) - Math.floor(this.radius) && Math.ceil(meal.y) - Math.floor(this.radius) < Math.floor(y) + Math.floor(this.radius) && key !==index[0]&&key !== index[1]&& key !== index[2]&& key !== index[3]);
+            return this.arrayOfPixels.find(({x, y}, key) => Math.floor(meal.x) + Math.floor(this.radius) > Math.ceil(x) - Math.floor(this.radius) && Math.ceil(meal.x) - Math.floor(this.radius) < Math.floor(x) + Math.floor(this.radius) && Math.floor(meal.y) + Math.floor(this.radius) > Math.ceil(y) - Math.floor(this.radius) && Math.ceil(meal.y) - Math.floor(this.radius) < Math.floor(y) + Math.floor(this.radius) && key !== index[0] && key !== index[1] && key !== index[2] && key !== index[3]);
         }
         if (this.arrayOfPixels.length === 0 || push) {
             this.arrayOfPixels.push({
@@ -63,7 +65,7 @@ class Game {
     }
 
     manage(direction) {
-        if ((direction === "ArrowRight" && this.direction[0] === "ArrowLeft" && this.direction.length <=1 || this.direction[0] === "ArrowRight" && direction === "ArrowLeft" && this.direction.length <=1 || direction === "ArrowUp" && this.direction[0] === "ArrowDown" && this.direction.length <=1 || this.direction[0] === "ArrowUp" && direction === "ArrowDown") && this.direction.length <=1 ||this.direction.length>2) {
+        if ((direction === "ArrowRight" && this.direction[0] === "ArrowLeft" && this.direction.length <= 1 || this.direction[0] === "ArrowRight" && direction === "ArrowLeft" && this.direction.length <= 1 || direction === "ArrowUp" && this.direction[0] === "ArrowDown" && this.direction.length <= 1 || this.direction[0] === "ArrowUp" && direction === "ArrowDown") && this.direction.length <= 1 || this.direction.length > 2) {
             return;
         }
         this.direction.push(direction);
@@ -71,9 +73,9 @@ class Game {
     }
 
     animation = () => {
-       if (this.stop){
-           return;
-       }
+        if (this.stop) {
+            return;
+        }
         if (!this.meal) {
             this.meal = this.mealRandomDraw();
         }
@@ -81,7 +83,7 @@ class Game {
         if (currentTime - this.startAnimationTime >= 500 * this.acceleration) {
             let oldX = this.x;
             let oldY = this.y;
-            if (this.direction.length>1){
+            if (this.direction.length > 1) {
                 this.direction.shift();
             }
             if (this.direction[0] === "ArrowRight") {
@@ -105,7 +107,7 @@ class Game {
             this.touchHelperDraw();
             this.snackDraw();
             this.mealRandomDraw();
-            const checkCrush = this.arraySnackManage("", "", "", "", false, true, [0,1,2,3], {
+            const checkCrush = this.arraySnackManage("", "", "", "", false, true, [0, 1, 2, 3], {
                 x: this.arrayOfPixels[0].x,
                 y: this.arrayOfPixels[0].y,
             });
@@ -119,10 +121,10 @@ class Game {
             }
             if (this.arraySnackManage("", "", "", "", false, true)) {
                 this.arraySnackManage("", "", this.x, this.y, true);
-                this.acceleration = this.acceleration-this.coeff;
+                this.acceleration = this.acceleration - this.coeff;
                 this.meal = null;
-               this.animation();
-               return;
+                this.animation();
+                return;
             }
             this.startAnimationTime = Date.now();
         }
@@ -178,108 +180,109 @@ class Game {
         }
         return {x: randomX, y: randomY};
     }
-    touchHelperDraw(){
-        if (!this.valuesNav){
+
+    touchHelperDraw() {
+        if (!this.valuesNav) {
             return;
         }
         this.ctx.beginPath();
-            this.ctx.fillStyle = "rgba(0, 0, 0, 0.08)"
-            this.ctx.moveTo(this.valuesNav.topTriangle.x1,this.valuesNav.topTriangle.y1);
-            this.ctx.lineTo(this.valuesNav.topTriangle.x2,this.valuesNav.topTriangle.y2);
-            this.ctx.lineTo(this.valuesNav.topTriangle.x3,this.valuesNav.topTriangle.y3);
-            this.ctx.moveTo(this.valuesNav.rightTriangle.x1,this.valuesNav.rightTriangle.y1);
-            this.ctx.lineTo(this.valuesNav.rightTriangle.x2,this.valuesNav.rightTriangle.y2);
-            this.ctx.lineTo(this.valuesNav.rightTriangle.x3,this.valuesNav.rightTriangle.y3);
-            this.ctx.moveTo(this.valuesNav.bottomTriangle.x1,this.valuesNav.bottomTriangle.y1);
-            this.ctx.lineTo(this.valuesNav.bottomTriangle.x2,this.valuesNav.bottomTriangle.y2);
-            this.ctx.lineTo(this.valuesNav.bottomTriangle.x3,this.valuesNav.bottomTriangle.y3);
-            this.ctx.moveTo(this.valuesNav.leftTriangle.x1,this.valuesNav.leftTriangle.y1);
-            this.ctx.lineTo(this.valuesNav.leftTriangle.x2,this.valuesNav.leftTriangle.y2);
-            this.ctx.lineTo(this.valuesNav.leftTriangle.x3,this.valuesNav.leftTriangle.y3);
-            this.ctx.fill();
+        this.ctx.fillStyle = "rgba(0, 0, 0, 0.08)"
+        this.ctx.moveTo(this.valuesNav.topTriangle.x1, this.valuesNav.topTriangle.y1);
+        this.ctx.lineTo(this.valuesNav.topTriangle.x2, this.valuesNav.topTriangle.y2);
+        this.ctx.lineTo(this.valuesNav.topTriangle.x3, this.valuesNav.topTriangle.y3);
+        this.ctx.moveTo(this.valuesNav.rightTriangle.x1, this.valuesNav.rightTriangle.y1);
+        this.ctx.lineTo(this.valuesNav.rightTriangle.x2, this.valuesNav.rightTriangle.y2);
+        this.ctx.lineTo(this.valuesNav.rightTriangle.x3, this.valuesNav.rightTriangle.y3);
+        this.ctx.moveTo(this.valuesNav.bottomTriangle.x1, this.valuesNav.bottomTriangle.y1);
+        this.ctx.lineTo(this.valuesNav.bottomTriangle.x2, this.valuesNav.bottomTriangle.y2);
+        this.ctx.lineTo(this.valuesNav.bottomTriangle.x3, this.valuesNav.bottomTriangle.y3);
+        this.ctx.moveTo(this.valuesNav.leftTriangle.x1, this.valuesNav.leftTriangle.y1);
+        this.ctx.lineTo(this.valuesNav.leftTriangle.x2, this.valuesNav.leftTriangle.y2);
+        this.ctx.lineTo(this.valuesNav.leftTriangle.x3, this.valuesNav.leftTriangle.y3);
+        this.ctx.fill();
         this.ctx.closePath();
     }
 }
-select.addEventListener("change",(e)=> {
+
+select.addEventListener("change", (e) => {
     select.classList.add("dn");
     canvas.classList.remove("dn");
     const game = new Game(ctx, canvasWidth, canvasHeight, radius, e.target.value);
     let pageX = canvasWidth;
     let pageY = canvasHeight;
     let triangles;
-    if (pageY>pageX){
-       const diagonal = pageY/3;
-       const diagonalHalf = diagonal/2;
+    if (pageY > pageX) {
+        const diagonal = pageY / 3;
+        const diagonalHalf = diagonal / 2;
         triangles = {
-            topTriangle:{
-                x1:pageX/2,
-                y1:pageY-diagonal,
-                x2: pageX/2-diagonalHalf/2,
-                y2: (pageY-diagonal)+diagonalHalf/2,
-                x3: pageX/2+diagonalHalf/2,
-                y3: (pageY-diagonal)+diagonalHalf/2,
+            topTriangle: {
+                x1: pageX / 2,
+                y1: pageY - diagonal,
+                x2: pageX / 2 - diagonalHalf / 2,
+                y2: (pageY - diagonal) + diagonalHalf / 2,
+                x3: pageX / 2 + diagonalHalf / 2,
+                y3: (pageY - diagonal) + diagonalHalf / 2,
             },
-            rightTriangle:{
-                x1:pageX/2+diagonalHalf/2,
-                y1:(pageY-diagonal)+diagonalHalf/2,
-                x2: pageX/2+diagonalHalf,
-                y2: (pageY-diagonal)+diagonalHalf,
-                x3: pageX/2+diagonalHalf/2,
-                y3: (pageY-diagonal)+diagonalHalf+diagonalHalf/2,
+            rightTriangle: {
+                x1: pageX / 2 + diagonalHalf / 2,
+                y1: (pageY - diagonal) + diagonalHalf / 2,
+                x2: pageX / 2 + diagonalHalf,
+                y2: (pageY - diagonal) + diagonalHalf,
+                x3: pageX / 2 + diagonalHalf / 2,
+                y3: (pageY - diagonal) + diagonalHalf + diagonalHalf / 2,
             },
-            bottomTriangle:{
-                x1:pageX/2+diagonalHalf/2,
-                y1:(pageY-diagonal)+diagonalHalf+diagonalHalf/2,
-                x2: pageX/2,
+            bottomTriangle: {
+                x1: pageX / 2 + diagonalHalf / 2,
+                y1: (pageY - diagonal) + diagonalHalf + diagonalHalf / 2,
+                x2: pageX / 2,
                 y2: pageY,
-                x3:  pageX/2-diagonalHalf/2,
-                y3: pageY-diagonalHalf/2,
+                x3: pageX / 2 - diagonalHalf / 2,
+                y3: pageY - diagonalHalf / 2,
             },
-            leftTriangle:{
-                x1:pageX/2-diagonalHalf/2,
-                y1:pageY-diagonalHalf/2,
-                x2: pageX/2-diagonalHalf,
-                y2: pageY-diagonalHalf,
-                x3:  pageX/2-diagonalHalf/2,
-                y3: (pageY-diagonal)+diagonalHalf/2,
+            leftTriangle: {
+                x1: pageX / 2 - diagonalHalf / 2,
+                y1: pageY - diagonalHalf / 2,
+                x2: pageX / 2 - diagonalHalf,
+                y2: pageY - diagonalHalf,
+                x3: pageX / 2 - diagonalHalf / 2,
+                y3: (pageY - diagonal) + diagonalHalf / 2,
             }
         }
-    }
-    else {
-        const diagonal = pageX/3;
-        const diagonalHalf = diagonal/2;
+    } else {
+        const diagonal = pageX / 3;
+        const diagonalHalf = diagonal / 2;
         triangles = {
-            leftTriangle:{
-                x1:(pageX-diagonal),
-                y1:pageY/2,
-                x2: (pageX-diagonal+diagonalHalf/2),
-                y2: pageY/2+diagonalHalf/2,
-                x3: (pageX-diagonal+diagonalHalf/2),
-                y3: pageY/2-diagonalHalf/2,
+            leftTriangle: {
+                x1: (pageX - diagonal),
+                y1: pageY / 2,
+                x2: (pageX - diagonal + diagonalHalf / 2),
+                y2: pageY / 2 + diagonalHalf / 2,
+                x3: (pageX - diagonal + diagonalHalf / 2),
+                y3: pageY / 2 - diagonalHalf / 2,
             },
-            topTriangle:{
-                x1:(pageX-diagonal+diagonalHalf/2),
-                y1:pageY/2-diagonalHalf/2,
-                x2: (pageX-diagonal+diagonalHalf),
-                y2: pageY/2-diagonalHalf,
-                x3: (pageX-diagonalHalf+diagonalHalf/2),
-                y3: pageY/2-diagonalHalf/2,
+            topTriangle: {
+                x1: (pageX - diagonal + diagonalHalf / 2),
+                y1: pageY / 2 - diagonalHalf / 2,
+                x2: (pageX - diagonal + diagonalHalf),
+                y2: pageY / 2 - diagonalHalf,
+                x3: (pageX - diagonalHalf + diagonalHalf / 2),
+                y3: pageY / 2 - diagonalHalf / 2,
             },
-            rightTriangle:{
-                x1:(pageX-diagonalHalf+diagonalHalf/2),
-                y1:pageY/2-diagonalHalf/2,
+            rightTriangle: {
+                x1: (pageX - diagonalHalf + diagonalHalf / 2),
+                y1: pageY / 2 - diagonalHalf / 2,
                 x2: pageX,
-                y2: pageY/2,
-                x3: (pageX-diagonalHalf+diagonalHalf/2),
-                y3: pageY/2+diagonalHalf/2,
+                y2: pageY / 2,
+                x3: (pageX - diagonalHalf + diagonalHalf / 2),
+                y3: pageY / 2 + diagonalHalf / 2,
             },
-            bottomTriangle:{
-                x1:(pageX-diagonalHalf+diagonalHalf/2),
-                y1:pageY/2+diagonalHalf/2,
-                x2: (pageX-diagonalHalf),
-                y2: pageY/2+diagonalHalf,
-                x3:  (pageX-diagonal+diagonalHalf/2),
-                y3: pageY/2+diagonalHalf/2,
+            bottomTriangle: {
+                x1: (pageX - diagonalHalf + diagonalHalf / 2),
+                y1: pageY / 2 + diagonalHalf / 2,
+                x2: (pageX - diagonalHalf),
+                y2: pageY / 2 + diagonalHalf,
+                x3: (pageX - diagonal + diagonalHalf / 2),
+                y3: pageY / 2 + diagonalHalf / 2,
             },
         }
     }
@@ -308,30 +311,28 @@ select.addEventListener("change",(e)=> {
         game.valuesNav = triangles;
         const touchX = e.touches[0].pageX;
         const touchY = e.touches[0].pageY;
-        if (pageY>pageX){
-            if (touchY>triangles.topTriangle.y1&&touchY<triangles.topTriangle.y2&&touchX>triangles.topTriangle.x2&&touchX<triangles.topTriangle.x3){
+        if (pageY > pageX) {
+            if (touchY > triangles.topTriangle.y1 && touchY < triangles.topTriangle.y2 && touchX > triangles.topTriangle.x2 && touchX < triangles.topTriangle.x3) {
                 game.manage("ArrowUp");
 
             }
-            if (touchY>triangles.leftTriangle.y3&&touchY<triangles.leftTriangle.y1&&touchX>triangles.leftTriangle.x2&&touchX<triangles.leftTriangle.x1){
+            if (touchY > triangles.leftTriangle.y3 && touchY < triangles.leftTriangle.y1 && touchX > triangles.leftTriangle.x2 && touchX < triangles.leftTriangle.x1) {
                 game.manage("ArrowLeft");
             }
-        }
-        else {
-            if (touchY>triangles.topTriangle.y2&&touchY<triangles.topTriangle.y1&&touchX>triangles.topTriangle.x1&&touchX<triangles.topTriangle.x3){
+        } else {
+            if (touchY > triangles.topTriangle.y2 && touchY < triangles.topTriangle.y1 && touchX > triangles.topTriangle.x1 && touchX < triangles.topTriangle.x3) {
                 game.manage("ArrowUp");
             }
 
-            if (touchY>triangles.leftTriangle.y3&&touchY<triangles.leftTriangle.y2&&touchX>triangles.leftTriangle.x1&&touchX<triangles.leftTriangle.x2){
+            if (touchY > triangles.leftTriangle.y3 && touchY < triangles.leftTriangle.y2 && touchX > triangles.leftTriangle.x1 && touchX < triangles.leftTriangle.x2) {
                 game.manage("ArrowLeft");
             }
         }
-        if (touchY>triangles.rightTriangle.y1&&touchY<triangles.rightTriangle.y3&&touchX>triangles.rightTriangle.x1&&touchX<triangles.rightTriangle.x2){
+        if (touchY > triangles.rightTriangle.y1 && touchY < triangles.rightTriangle.y3 && touchX > triangles.rightTriangle.x1 && touchX < triangles.rightTriangle.x2) {
             game.manage("ArrowRight");
         }
-        if (touchY>triangles.bottomTriangle.y1&&touchY<triangles.bottomTriangle.y2&&touchX>triangles.bottomTriangle.x3&&touchX<triangles.bottomTriangle.x1){
+        if (touchY > triangles.bottomTriangle.y1 && touchY < triangles.bottomTriangle.y2 && touchX > triangles.bottomTriangle.x3 && touchX < triangles.bottomTriangle.x1) {
             game.manage("ArrowDown");
-
         }
     })
 })
